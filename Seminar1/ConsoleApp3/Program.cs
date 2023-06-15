23980fd56867205d0d586e91a11a7906f0ea8d6f
Janara <b_090_jan@mail.ru>
2023-06-15 21:09:57 +0400

    Dom zadanie3

---
+++ Seminar1/ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Enter the number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = num % 2;
+
+if (remDiv == 0)
+{
+    Console.WriteLine ("Yes");
+}
+else
+{
+    Console.WriteLine ("No");
+}
