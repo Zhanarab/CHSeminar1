fc8707ff65e550407c863369aa030cb42ae9254a
Janara <b_090_jan@mail.ru>
2023-06-15 17:04:54 +0400

    dom zadanie 1

---
+++ Seminar1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// 
+
+int max = 0;
+int min = 0;
+
+
+
+Console.Write("Enter first number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Enter second number:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number > number2)
+{
+    max = number;
+    min = number2;
+}
+else 
+{
+    max = number2;
+    min = number;
+}
+
+Console.WriteLine("max =" + max + "and" + "min = " + min);
+
+
+
