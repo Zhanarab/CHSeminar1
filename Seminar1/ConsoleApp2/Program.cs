838b0c2bb657e83e30de28c93a3ff0837c4bf126
Janara <b_090_jan@mail.ru>
2023-06-15 17:47:06 +0400

    Dom Zadanie 2

---
+++ Seminar1/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на вход
+// три числа и выдаёт максимальное из этих чисел.
+
+int max = 0;
+
+
+Console.Write("Enter first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter third number:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > max)
+{
+    max = num1;
+}
+if(num2 > max)
+{
+    max = num2;
+}
+if(num3 > max)
+{
+    max = num3;
+}
+
+Console.WriteLine("max =" + max);
